cb5b12d2d4005fc4158d6b47115fff8cfeea64aa
jona8690 <jona8690@edu.eal.dk>
2017-02-02 14:25:29 +0100

    Stated fibo class

---
+++ Project Euler/Core/Fibonacci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core {
+	class Fibonacci {
+		public double MaxValue { get; set; }
+		public double MaxTerm { get; set; }
+
+		private double CurrentTerm = 1;
+		private double CurrentValue = 1;
+		private double LastValue = 1;
+
+		public void Run() {
+
+		}
+	}
+}
